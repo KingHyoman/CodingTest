841b12d1d2d0ae3f817b225e09c97f9076f4a4de
KingHyoman <sjrygsjw@gmail.com>
2024-06-13 20:51:29 +0900

    Jun 11, 204

---
+++ Programmers/maxmin_2.py
@@ -0,0 +1,17 @@
+# Find the max and min from given string
+# ex) "1 2 3 4 5"
+# return "1 5"
+
+def solution(s):
+    tem = ''
+    arr = []
+    for ch in s:
+        if ch != ' ':
+            tem += ch
+        else:
+            arr.append(int(tem))
+            tem = ''
+    arr.append(int(tem))
+    return f'{min(arr)} {max(arr)}'
+
+print(solution("-1 -1"))
