0e8d5a7df996bfab671dfdee58584fec86667135
KingHyoman <sjrygsjw@gmail.com>
2024-07-02 11:44:03 +0900

    Jul 1, 2024

--- Programmers/candidatekey_2.py
+++ Programmers/candidatekey_2.py
@@ -64,11 +64,11 @@ def solution(relation):
 print(solution([["100","ryan","music","2"],["200","apeach","math","2"],\
                 ["300","tube","computer","3"],["400","con","computer","4"],\
                     ["500","muzi","music","3"],["600","apeach","music","2"]]))
-
+# Second test case: 3
 print(solution([["100","100","ryan","music","2"], \
                          ["200","200","apeach","math","2"], ["300","300","tube","computer","3"], \
                             ["400","400","con","computer","4"], ["500","500","muzi","music","3"], \
                                 ["600","600","apeach","music","2"]]))
-
+# Third test case: 5
 print(solution([["a","1","aaa","c","ng"], ["a","1","bbb","e","g"],\
                 ["c","1","aaa","d","ng"], ["d","2","bbb","d","ng"]]))
