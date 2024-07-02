a7d596d340dd3ccfeeab6741ed4a958f4c0965dc
KingHyoman <sjrygsjw@gmail.com>
2024-07-02 11:43:31 +0900

    Jul 1, 2024

--- Programmers/candidatekey_2.py
+++ Programmers/candidatekey_2.py
@@ -60,6 +60,7 @@ def solution(relation):
                           
     return len(answers)
 
+# First test case: 2
 print(solution([["100","ryan","music","2"],["200","apeach","math","2"],\
                 ["300","tube","computer","3"],["400","con","computer","4"],\
                     ["500","muzi","music","3"],["600","apeach","music","2"]]))
